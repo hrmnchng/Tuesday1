01bc616b995f8d21848f7162b19d6fd0f11f1587
herman <hahaherman@gmail.com>
2017-09-19 11:19:07 -0400

    Week 3
    attempted to do lab # 2

---
+++ Source/Tuesday1/Tuesday1/Initialiaztion.h
@@ -0,0 +1,103 @@
+#include <windows.h>
+#include <stdio.h>
+
+#include <direct.h>
+#include <winnt.h>
+
+#ifdef UNICODE
+	typedef LPCWSTR LPCTSTR;
+#else
+	typedef LPCTSTR LPCTSTR;
+#endif
+
+
+typedef CONST WCHAR *LPCWSTR;
+
+class Init 
+{
+
+public:
+
+
+	typedef void *PVOID;
+	typedef  unsigned __int64 DWORDLONG;
+	typedef unsigned long DWORD;
+	typedef PVOID HANDLE;
+	
+	
+		
+
+private:
+
+	bool IsOnlyInstance(LPCTSTR gameTitle) 
+		{
+			HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+			if (GetLastError() != ERROR_SUCCESS) {
+				HWND hWnd = FindWindow(gameTitle, NULL);
+				if (hWnd) {
+					// An instance of your game is already running.
+					ShowWindow(hWnd, SW_SHOWNORMAL);
+					SetFocus(hWnd);
+					SetForegroundWindow(hWnd);
+					SetActiveWindow(hWnd);
+					return false;
+				}
+			}
+			return true;
+		}
+
+	bool CheckStorage(const DWORDLONG diskSpaceNeeded) 
+	{
+		// Check for enough free disk space on the current disk.
+		int const drive = _getdrive();
+		struct _diskfree_t diskfree;
+		_getdiskfree(drive, &diskfree);
+		unsigned __int64 const neededClusters = diskSpaceNeeded /
+			(diskfree.sectors_per_cluster*diskfree.bytes_per_sector);
+		if (diskfree.avail_clusters < neededClusters) {
+			// if you get here you don’t have enough disk space!
+			printf("CheckStorage Failure : Not enough physical storage.");
+			return false;
+		}
+		return true;
+	}
+
+	bool CheckMemory(const DWORDLONG physicalRAMNeeded, const	DWORDLONG virtualRAMNeeded)
+	{
+		MEMORYSTATUSEX status;
+		GlobalMemoryStatusEx(&status);
+		if (status.ullTotalPhys < physicalRAMNeeded) {
+
+			printf("CheckMemory Failure : Not enough physical memory.");
+				return false;
+		}
+		// Check for enough free memory.
+		if (status.ullAvailVirtual < virtualRAMNeeded)
+		{
+			printf("CheckMemory Failure : Not enough virtual	memory.");
+			return false;
+
+		}
+	}
+
+	DWORD ReadCPUSpeed()
+	{
+		DWORD BufSize = sizeof(DWORD);
+		DWORD dwMHz = 0;
+		DWORD type = REG_DWORD;
+		HKEY hKey;
+		// open the key where the proc speed is hidden:
+		long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE,L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0,
+			KEY_READ, &hKey);
+		if (lError == ERROR_SUCCESS)
+		{
+			// query the key:
+			RegQueryValueEx(hKey, L"MHz", NULL, &type, (LPBYTE)
+				&dwMHz, &BufSize);
+		}
+		return dwMHz;
+	}
+
+
+};
+
--- Source/Tuesday1/Tuesday1/Source.cpp
+++ Source/Tuesday1/Tuesday1/Source.cpp
@@ -1,10 +1,56 @@
 
 #include <string>
+#include "Initialiaztion.h"
 
 int main()
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	INT WINAPI GameCode4
+
+	(
+		HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPWSTR lpCmdLine,
+		int nCmdShow
+	)
+
+		{
+			int tmpDbgFlag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+			tmpDbgFlag j = _CRTDBG_LEAK_CHECK_DF;
+			_CrtSetDbgFlag(tmpDbgFlag);
+
+			g_pApp->m_Options.Init("PlayerOptions.xml", lpCmdLine);
+
+			if (g_pApp->m_Options.m_Renderer == "Direct3D 11")
+			{
+				DXUTSetCallbackD3D11DeviceAcceptable(GameCodeApp::IsD3D11DeviceAcceptable);
+				DXUTSetCallbackD3D11DeviceCreated(GameCodeApp::OnD3D11CreateDevice);
+				DXUTSetCallbackD3D11DeviceReset(GameCodeApp::OnD3D11ResetDevice);
+				DXUTSetCallbackD3D11DeviceLost(GameCodeApp::OnD3D11LostDevice);
+				DXUTSetCallbackD3D11DeviceDestroyed(GameCodeApp::OnD3D11DestroyDevice);
+				DXUTSetCallbackD3D11FrameRender(GameCodeApp::OnD3D11FrameRender);
+			}
+			else if (g_pApp->m_Options.m_Renderer == "Direct3D 12")
+			{
+				DXUTSetCallbackD3D12DeviceAcceptable(GameCodeApp::IsD3D12DeviceAcceptable);
+				DXUTSetCallbackD3D12DeviceCreated(GameCodeApp::OnD3D12CreateDevice);
+				DXUTSetCallbackD3D12SwapChainResized(GameCodeApp::OnD3D12ResizedSwapChain);
+				DXUTSetCallbackD3D12SwapChainReleasing(GameCodeApp::OnD3D12ReleasingSwapChain);
+				DXUTSetCallbackD3D12DeviceDestroyed(GameCodeApp::OnD3D12DestroyDevice);
+				DXUTSetCallbackD3D12FrameRender(GameCodeApp::OnD3D12FrameRender);
+			}
+			else
+			{
+				GCC_ASSERT(0 && "Unknown renderer specified in game options.");
+				return false;
+			}
+
+			if (!g_pApp->InitInstance(hInstance, lpCmdLine, 0,
+				g_pApp->m_Options.m_ScreenSize.x, g_pApp->m_Options.m_ScreenSize.y))
+			{
+				return FALSE;
+			}
+
+		}
 }
+
+
